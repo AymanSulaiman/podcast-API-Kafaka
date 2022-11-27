b0808eb8ee49b495d5050ec7b4a0599e557a6aa0
AymanSulaiman <saymansulaiman@gmail.com>
2022-11-26 17:57:51 -0800

    Added Kafka in fastapi, I think this is how you are supposed to do it

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
 from typing import Union
-
+import asyncio
 from fastapi import FastAPI
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from config import loop, KAFKA_BOOTSTRAP_SERVERS, KAFKA_CONSUMER_GROUP, KAFKA_TOPIC
 
 app = FastAPI()
 
@@ -27,39 +29,59 @@ def find_podcast_show(title):
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/podcast/episode_id/{_id}")
-def get_podcast_episode_id(_id:str):
+@app.post("/podcast/episode_id/{_id}")
+async def get_podcast_episode_id(_id:str):
+    producer = AIOKafkaProducer(
+        loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    await producer.start()
     try:
         return get_podcast_ep_data_id(_id)
     except:
         return {"There was":"nothing there"}
+    finally:
+        await producer.stop()
 
 @app.get("/podcast/episode_find/{title}")
-def get_podcast_episode_find(title:str):
+async def get_podcast_episode_find(title:str):
+    producer = AIOKafkaProducer(
+        loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    await producer.start()
     try:
         return find_podcast_ep(title)
     except:
         return {"There was":"nothing there"}
+    finally:
+        await producer.stop()
 
 
 @app.get("/podcast/show_id/{_id}")
-def get_podcast_show_id(_id:str):
+async def get_podcast_show_id(_id:str):
+    producer = AIOKafkaProducer(
+        loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    await producer.start()
     try:
         return get_podcast_show_data_id(_id)
     except:
         return {"There was":"nothing there"}
+    finally:
+        await producer.stop()
 
 @app.get("/podcast/show_find/{title}")
-def get_podcast_show_find(title:str):
+async def get_podcast_show_find(title:str):
+    producer = AIOKafkaProducer(
+        loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    await producer.start()
     try:
         return find_podcast_show(title)
     except:
         return {"There was":"nothing there"}
+    finally:
+        await producer.stop()
